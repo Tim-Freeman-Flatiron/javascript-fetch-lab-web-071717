54fd17bd9b2bbe2e2afd9b395af81c6a117aa912
Tim Freeman <d.timothy.freeman@gmail.com>
2017-09-11 16:07:29 -0400

    done

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 function getIssues() {
+	fetch('https://api.github.com/repos/dtfreemn/javascript-fetch-lab/issues',{
+  	headers: { Authorization: `token ${getToken()}`},
+	}).then(function(response) {return response.json()})
+		.then(function(json) { showIssues(json)})
 }
 
 function showIssues(json) {
+	let list = document.getElementById('issues')
+	json.forEach(function(issue) {
+		let div = `<div>${issue.body}</div>`
+		list.innerHTML += div
+	})
 }
 
 function createIssue() {
+	let issueTitle = document.getElementById('title').value
+	let issueBody = document.getElementById('body').value
+	fetch('https://api.github.com/repos/dtfreemn/javascript-fetch-lab/issues',{
+		method: 'post',
+  	headers: { Authorization: `token ${getToken()}`},
+  	body: JSON.stringify({title: issueTitle, body: issueBody})
+	}).then(function(response) {return response.json()})
+		.then(function(json) { getIssues(json)})
 }
 
 function showResults(json) {
+	let url = json.html_url
+	let div = `<a href="${url}" target="_blank">${url}</a>`
+	document.getElementById('results').innerHTML = div
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
+  fetch(repo, {
+  	method: 'post',
+  	headers: { Authorization: `token ${getToken()}`}
+  }).then(res => res.json()).then(json => showResults(json))
 }
 
 function getToken() {
